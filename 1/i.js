4765b68c0292c60d43caae94e65de76fca492b46
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-16 21:23:32 +0300

    2

---
+++ 1/i.js
@@ -0,0 +1,28 @@
+console.log('Labas, Afrika!');
+
+
+
+let message; // sukuriam kintamaji
+
+console.log(message);
+
+let pukuotaPilkaZasis = 44;
+
+
+
+message = 55; // kintamaj duodam reiksme
+
+message = '77'; // kintamaj duodam reiksme
+let message1 = 77; // kintamaj duodam reiksme
+let message2 = true;
+let message3 = null;
+
+
+console.log(typeof message);
+console.log(typeof message1);
+console.log(typeof message2);
+console.log('----------------------------');
+console.log(message3);
+console.log(message);
+console.log(message1);
+// console.log(pukuotaPilkaZasis);
---
+++ 2/i.js
@@ -0,0 +1,80 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// + - / * %
+let box1 = 8;
+let box2 = 3;
+let rezult = 10;
+
+// rezult = rezult + box1;
+rezult += box1;
+
+// rezult = rezult + 1;
+
+rezult = 10;
+
+let rezult1 = rezult++ + rezult++;
+
+rezult1++;
+
+// console.log(rezult1);
+// console.log(rezult);
+
+let box3 = 'labas';
+
+box3 = box3 * 1; // ---> NaN Not a Number
+
+// console.log(6 + 2)
+
+// console.log(Number('55'));
+
+// console.log(Boolean(664645));
+
+
+// undefined, Infinity, NaN
+
+// console.log('3 > 5', 3 > 5);
+// console.log('3 < 5', 3 < 5);
+// console.log('3 != 5', 3 != 5);
+
+// console.log('Z > A', 'Z' > 'A');
+// console.log('z > Z', 'z' > 'Z');
+// console.log('z < z123', 'z' < 'z123');
+
+// console.log(rand(5, 10));
+
+let check = rand(1, 9);
+
+if (check < 5) {
+
+    if (8 < 5) {
+
+    }
+    console.log('TAIP!');
+    console.log(check);
+
+} else if (check == 5) {
+
+    console.log('Lygu!');
+    console.log(check);
+
+} else {
+
+    console.log('NE!');
+    console.log(check);
+}
+
+
+console.log('Ate!');
+
+let r1 = rand(0, 4);
+let r2 = rand(0, 4);
+
+
+
+if (2 < 5 && 6 < 9) {} //1 && 1 => true
+if (2 < 5 || 6 < 9) {} // 0 || 1 , 1 || 0 => true
