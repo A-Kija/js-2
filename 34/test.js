9e73f6c0ddbef885a7e48e8fa9ff0bb6777e2d30
A-Kija <arvydas.kijakauskas@gmail.com>
2021-11-13 14:18:50 +0200

    lll

--- 34/test.js
+++ 34/test.js
@@ -22,7 +22,7 @@ const set1 = new Set(['Labas', 'Vakaras', 'Rytas', 'Valio']);
 
 //7. Suprototaipinti standartinį String tipo objektą, pridedant metodą lastCharIs(), 
 // kuris grąžintų paskutinį stringo simbolį. Pademonstruoti veikimą su stringu 'Ku Ku';
-const str2 = 'Ku ku';
+const str2 = new String('Ku ku');
 // console.log(str2.lastCharIs()) po prototaipinimo eilutė turi veikti
 
 //8. Duotas objektas, kurį reikia iteruoti, kiekvienoje iteracijoje išvedant savybę
@@ -31,3 +31,141 @@ const obj1 = { labas: 'vakaras', laba: 'diena', labs: 'ryts' };
 
 //9. Sukurti klasę Grybas. Klasėje sukurti savybė tipas. Iš klasės sukurti
 // 3 Grybo objektus, savybei tipas priskiriant "Baravykas", "Ūmedė" ir "Lepšis"
+
+mas4.forEach((s, i) => {
+    if (s[0] == 'a' || s[0] == 'A' || s[0] == 'Ą') {
+        console.log(i, s);
+    }
+});
+
+set1.add("Labas");
+set1.add("Ate");
+console.log(set1, set1.size);
+
+String.prototype.lastCharIs = function() {
+    return this.valueOf()[this.valueOf().length - 1];
+}
+
+String.prototype.lastCharIs = function() {
+    return 'Valio';
+}
+
+console.log(str2.lastCharIs());
+console.log(str2);
+
+
+const map = new Map([
+    ['a', 'labas'],
+    ['b', 'ate'],
+    ['x', 'xxx']
+]);
+
+console.log(map);
+
+
+
+
+
+
+class A {
+    static labas() {
+        for (const s of map) {
+            console.log(s[0], s[1]);
+        }
+    }
+}
+
+A.labas();
+
+// console.log(s);
+
+// map.forEach(a => console.log(a));
+
+class Grybas {
+    tipas;
+
+    constructor(tipas) {
+        this.tipas = tipas;
+    }
+}
+
+const G = new Grybas("Baravykas");
+
+
+
+Grybas.prototype.Tipas = function() {
+    return this.tipas;
+}
+
+
+console.log(G.Tipas());
+
+console.log(
+    new Grybas("Baravykas"),
+    new Grybas("Ūmedė"),
+    new Grybas("Lepšis"),
+);
+
+class C {
+
+    constructor() {
+        this.nu = this.nu.bind(this);
+    }
+
+    sayHello() {
+        console.log('CCCCCCCCCCCC');
+    }
+
+    nu() {
+        this.sayHello();
+        // console.log(this);
+    }
+
+}
+
+const bObjC = new C();
+
+
+class B {
+
+    constructor(c) {
+        this.nu = this.nu.bind(c);
+    }
+
+    sayHello() {
+        console.log('Nu, Hello');
+        return function() {
+            console.log('super');
+        }
+    }
+
+    nu() {
+        this.sayHello();
+        // console.log(this);
+    }
+
+}
+
+const bObj = new B(bObjC);
+
+
+
+
+
+
+
+
+document.querySelector('button').addEventListener('click', bObj.nu);
+
+
+function abc(call) {
+    for (let i = 0; i < 13; i++) {
+        call();
+    }
+}
+
+
+
+// abc(bObj.nu);
+
+abc(bObj.sayHello);
